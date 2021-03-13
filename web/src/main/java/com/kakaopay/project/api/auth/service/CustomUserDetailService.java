cec451c0d357fa2fc783e4117b2805616a3b6db5
WhiteCodeLaboratory <namhanjunsul@naver.com>
2021-03-13 22:55:03 +0900

    component scan 범위수정
    component scan 범위수정

--- web/src/main/java/com/kakaopay/project/api/auth/service/CustomUserDetailService.java
+++ web/src/main/java/com/kakaopay/project/api/auth/service/CustomUserDetailService.java
@@ -1,29 +1,35 @@
 package com.kakaopay.project.api.auth.service;
 
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import com.kakaopay.project.api.member.dto.MemberDetailDto;
+import com.kakaopay.project.api.member.mapper.MemberMapper;
+
 @Service
 public class CustomUserDetailService implements UserDetailsService {
 
-  // private final MemberMapper memberMapper;
-  //
-  // @Autowired
-  // public CustomUserDetailService(MemberMapper memberMapper) {
-  // this.memberMapper = memberMapper;
-  // }
+  private final MemberMapper memberMapper;
+
+  @Autowired
+  public CustomUserDetailService(MemberMapper memberMapper) {
+    this.memberMapper = memberMapper;
+  }
 
   @Override
   public UserDetails loadUserByUsername(String memberId) throws UsernameNotFoundException {
-    // MemberDetailDto memberDetailDto = memberMapper.selectMemberById(Long.valueOf(memberId));
-    // if (memberDetailDto != null && memberId.equals(String.valueOf(memberDetailDto.getMemberId()))) {
-    // return new User(String.valueOf(memberDetailDto.getMemberId()), memberDetailDto.getPassword(), new ArrayList<>());
-    // } else {
-    // throw new UsernameNotFoundException("Member not found!!");
-    // }
-    return null;
+    MemberDetailDto memberDetailDto = memberMapper.selectMemberById(Long.valueOf(memberId));
+    if (memberDetailDto != null && memberId.equals(String.valueOf(memberDetailDto.getMemberId()))) {
+      return new User(String.valueOf(memberDetailDto.getMemberId()), memberDetailDto.getPassword(), new ArrayList<>());
+    } else {
+      throw new UsernameNotFoundException("Member not found!!");
+    }
   }
 
 }
